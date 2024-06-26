907943b416b68879fc89cf6c482451874e835154
Hazem <hzm@gmail.com>
2024-06-26 18:13:24 +0300

    first version

---
+++ lib/views/home_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/widgets/categories_list_view.dart';
+import 'package:news_app_ui_setup/widgets/custom_app_bar.dart';
+
+import '../widgets/news_list_view_builder.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            CustomAppBar(),
+            Expanded(
+              child: CustomScrollView(
+                physics: BouncingScrollPhysics(),
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: CategoriesListView(),
+                  ),
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 26,
+                    ),
+                  ),
+                  NewsListViewBuilder(
+                    category: 'general',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
